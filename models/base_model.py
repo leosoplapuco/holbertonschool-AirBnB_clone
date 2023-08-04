c22bcea198348009f04c028ae01465a91274cca9
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-08-04 12:33:53 -0700

    models 11

---
+++ models/base_model.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""módulo de la Base"""
+
+import uuid
+from datetime import datetime
+from uuid import uuid4
+import models
+import json
+
+format_dt = "%Y-%m-%dT%H:%M:%S.%f"
+
+
+class BaseModel:
+    """clase Basemodel"""
+    def __init__(self, *args, **kwargs):
+        """Database"""
+        if args is not None and len(args) > 0:
+            pass
+        if kwargs:
+            for key, item in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    item = datetime.strptime(item, format_dt)
+                if key not in ['__class__']:
+                    setattr(self, key, item)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def to_dict(self):
+        """definición para to_dict"""
+        
+        dic = {}
+        for key, item in self.__dict__.items():
+            if key in ['created_at', 'updated_at']:
+                dic[key] = item
+
+        dic['__class__'] = self.__class__.__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
+        return dic
+
+
+    def __str__(self):
+        """definición para str"""
+        return("[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__))
+
+    def save(self):
+        """definición para Save"""
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
