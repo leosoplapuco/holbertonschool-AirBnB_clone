261efe33061488a7400f3e3aa5a03dc41619c798
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-08-04 12:58:38 -0700

    models 13

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""clase City"""
+
+import uuid
+from datetime import datetime
+from models import storage
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """clase City"""
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""clase State"""
+
+import uuid
+from datetime import datetime
+from models import storage
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """clase State"""
+    name = ""
