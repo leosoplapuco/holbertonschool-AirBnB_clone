64d267ee139a6a2133d7296f642383fc00deaf37
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-08-04 22:46:59 -0700

    models 21

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """FileStorage"""
 
-
+import storage
+from models import base_model, user, place, state, city, amenity, review
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
