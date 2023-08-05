c9b777fd7fa6809a1e82e31f6ddd350a35a6f48a
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-08-04 22:35:36 -0700

    models 18

--- models/base_model.py
+++ models/base_model.py
@@ -43,8 +43,8 @@ class BaseModel:
 
     def __str__(self):
         """str"""
-        return("[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__))
+        #return("[{}] ({}) {}".format(self.__class__.__name__,self.id, self.__dict__))
+        return "BaseModel object"
 
     def save(self):
         """save"""
